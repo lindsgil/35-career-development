e1855735c2e3ff1ff78f29270172dd767bba1b26
Benjamin <ayzenbergb@gmail.com>
2017-06-12 10:32:59 -0700

    functionality coded

---
+++ lab-ben/lib/DLL.js
@@ -0,0 +1,90 @@
+'use strict';
+
+const Node = function(val, key, next=null, prev=null) {
+  this.key = key;
+  this.val = val;
+  this.next = next;
+  this.prev = prev;
+};
+
+
+const DLL = module.exports =  function() {
+  this.head = null;
+  this.tail = null;
+  this.length = 0;
+};
+
+DLL.prototype.append = function(val, key) {
+  if(!val) throw new Error('Please provide a value');
+  if(!key) throw new Error('Please provide a key');
+  if(!this.head) return this.head = this.tail = new Node(val, key);
+
+  let node = new Node(val, key);
+
+  this.head.next = node;
+  node.prev = this.head;
+  this.head = this.head.next;
+  this.length++;
+  return this.head;
+};
+
+DLL.prototype.prepend = function(val, key) {
+  if(!val) throw new Error('Please provide a value');
+  if(!key) throw new Error('Please provide a key');
+  if(!this.tail) return this.tail = this.head = new Node(val, key);
+
+
+  let node = new Node(val, key);
+
+  this.tail.prev = node;
+  node.next = this.tail;
+  this.tail = this.tail.prev;
+  this.length++;
+  return this.tail;
+};
+
+DLL.prototype.find = function(key) {
+  if(!key) throw new Error('Please provide a key');
+
+  let _traverse = (node) => {
+    if(!node) throw new Error('Key not found');
+    if(node.key === key) return node.val;
+    _traverse(node.next);
+  };
+
+  _traverse(this.head);
+};
+
+DLL.prototype.remove = function(key) {
+  if(!key) throw new Error('Please provide a key');
+  if(!this.tail) throw new Error('the list is empty');
+
+  if(this.tail.key === key && this.head === this.tail) {
+    this.tail = null;
+    this.head = null;
+    return this.length--;
+  }
+
+  if(this.tail.key === key) {
+    this.tail = this.tail.prev;
+    this.tail.next = null;
+    return this.length--;
+  }
+
+  if(this.head.key === key) {
+    this.head = this.head.next;
+    this.head.prev = null;
+    return this.length--;
+  }
+
+  let _check = (node) => {
+    if(!node) return new Error('No Value found');
+    if(node.key === key) {
+      let temp = node.next;
+      node.next.prev = node.prev;
+      node.prev.next = temp;
+      return this.length--;
+    }
+    _check(node.prev);
+  };
+};
